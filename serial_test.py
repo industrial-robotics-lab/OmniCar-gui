270626984b47f2efcfb1adbfbee3ed01e2f926b0
Divelix <divelix1@gmail.com>
2020-11-11 16:22:03 +0300

    Added serial benchmark

--- serial_test.py
+++ serial_test.py
@@ -1,27 +1,36 @@
 #!/usr/bin/env python3
 import serial
-import time
-import numpy as np
 import struct
+import time
 
 port = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
 port.flush()
+times = []
+corrupted = 0
 
 
 def tx_rx_floats():
+    global corrupted
+    start = time.time()
     # TX
-    byte_array = struct.pack('4f', 123.123, 342.322, 783.39, 8478.49)
+    byte_array = struct.pack('4f', 1.1, 2.2, 3.3, 4.4)
     port.write(byte_array)
     # RX
     response = port.readline()
+    end = time.time() - start
+    times.append(end)
     if len(response) == 17:
         floats = struct.unpack('4f', response[0:16])
-        print(f"Received: {floats} size = {len(response)}")
+        if int(sum(floats)) != 11:
+            corrupted += 1
+        print(f"Received: {int(sum(floats))} size = {len(response)}, time = {end}")
     else:
         print(f"corrupted msg of size {len(response)}")
+        corrupted += 1
 
 
-for _ in range(15):
+for _ in range(100):
     tx_rx_floats()
 
+print(f"FPS: {1 / (sum(times)/len(times))} ({len(times)} msgs, {corrupted} corrupted)")
 port.close()
