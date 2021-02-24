3c93433389b5682b3ea255e83a1797ee978baed5
Divelix <divelix1@gmail.com>
2021-02-24 16:35:39 +0300

    Functioning plotting

--- main.py
+++ main.py
@@ -5,56 +5,38 @@ from threading import Thread
 from omni import *
 import time
 
-theta = []
-x = []
-y = []
-
 def switch_vels():
     for vel in car_vels:
         car.set_car_velocity(vel)
         print(f"Change vel to {vel}")
         time.sleep(2)
+    # plt.close()
 
-def get_feedback():
-    while not car.is_stop():
-        vel = car.get_velocity()
-        vel = vel.ravel().tolist()
-        # print(f"vel: {vel}")
-        theta.append(vel[0])
-        x.append(vel[1])
-        y.append(vel[2])
-        time.sleep(0.1)
 
 def animate(i):
     plt.cla() # clear axis (keep line the same color on each iteration)
-    plt.plot(x, y)
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.plot(car.x, car.y)
 
-ani = FuncAnimation(plt.gcf(), animate, interval=1000) # get current figure
+ani = FuncAnimation(plt.gcf(), animate, interval=500) # get current figure
 
 switcher = Thread(target=switch_vels)
-feedbacker = Thread(target=get_feedback)
 
 car_vels = [
-    [0, 0.05, 0],
-    [0, 0, 0.05],
-    [0.5, 0, 0],
-    [0, 0.05, 0],
     [0, 0, 0],
+    [0, 0.05, 0],
+    [0, 0, 0]
 ]
 
-car = Car(10, 20, 0.0589/2)
+car = Car()
 
 car.start_arduino_talk()
 switcher.start()
-feedbacker.start()
 
 plt.tight_layout()
 plt.show()
 
 switcher.join()
 car.stop_arduino_talk()
-feedbacker.join()
-print(f"theta: {len(theta)}; x: {len(x)}; y: {len(y)}")
-# print(f"theta: {theta}")
-# print(f"x: {x}")
-# print(f"y: {y}")
+print(f"x: {len(car.x)}; y: {len(car.y)}")
--- omni.py
+++ omni.py
@@ -1,4 +1,4 @@
-from omnimath import *
+from omnimath import vec6_to_SE3, np
 import serial
 import struct
 from threading import Thread
@@ -7,13 +7,16 @@ from threading import Thread
 class Car:
     max_velocity = 10
 
-    def __init__(self, w, l, r):
+    def __init__(self):
         self._desired_velocity = np.zeros([3, 1])
         self._feedback_velocity = np.zeros([3, 1])
-        
-        self.w = w
-        self.l = l
-        self.r = r
+
+        self._twist = np.zeros([6, 1])
+        self._prev_to_current_pose = np.eye(4)
+        self._global_pose = np.eye(4)
+
+        self.x = []
+        self.y = []
 
         self._port = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
         self._port.flush()
@@ -36,7 +39,8 @@ class Car:
 
     # ------------- Arduino ----------------
     def tx_velocity(self):
-        byte_array = struct.pack('3f', self._desired_velocity[0], self._desired_velocity[1], self._desired_velocity[2])
+        byte_array = struct.pack(
+            '3f', self._desired_velocity[0], self._desired_velocity[1], self._desired_velocity[2])
         self._port.write(byte_array)
         self._port.write(b'\n')
 
@@ -48,7 +52,15 @@ class Car:
                 self._feedback_velocity[0] = feedback[0]
                 self._feedback_velocity[1] = feedback[1]
                 self._feedback_velocity[2] = feedback[2]
-                # print(f"feedback: {feedback}")
+
+                self._twist[2] = feedback[0]
+                self._twist[3] = feedback[1]
+                self._twist[4] = feedback[2]
+                self._prev_to_current_pose = vec6_to_SE3(self._twist)
+                self._global_pose = np.dot(self._global_pose, self._prev_to_current_pose)
+                # print(f"feedback: {feedback}\nTwist: {self._twist}\nGlobal pos: {self._global_pose}")
+                self.x.append(self._global_pose[0,3])
+                self.y.append(self._global_pose[1,3])
         else:
             self._corrupt_count += 1
 
@@ -68,7 +80,8 @@ class Car:
     def stop_arduino_talk(self):
         self._is_stop = True
         self._arduino_thread.join()
-        print(f"Arduino communication stopped after {self._msg_count} messages ({self._corrupt_count} corrupted)")
+        print(
+            f"Arduino communication stopped after {self._msg_count} messages ({self._corrupt_count} corrupted)")
         self._msg_count = 0
         self._corrupt_count = 0
 
