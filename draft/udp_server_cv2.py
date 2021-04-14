b1d60304829145226c9b0eacd27be8de2b0a09c0
Divelix <divelix1@gmail.com>
2021-04-14 16:47:10 +0300

    Added fps to image

--- draft/udp_server_cv2.py
+++ draft/udp_server_cv2.py
@@ -4,7 +4,7 @@ import time
 import base64
 
 BUFF_SIZE = 65536
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, BUFF_SIZE)
 host_name = socket.gethostname()
 host_ip = socket.gethostbyname(host_name)
@@ -27,8 +27,17 @@ while True:
         encoded, buffer = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 80])
         message = base64.b64encode(buffer)
         server_socket.sendto(message, client_addr)
+        frame = cv2.putText(frame, f"Server FPS: {str(fps)}", (10,40), cv2.FONT_HERSHEY_DUPLEX, 0.7, (0,0,255), 2)
         cv2.imshow("TRANSMITTING VIDEO", frame)
         key = cv2.waitKey(1) & 0xFF
         if key == ord('q'):
             server_socket.close()
             break
+        if cnt == frames_to_count:
+            try:
+                fps = round(frames_to_count/(time.time()-st))
+                st = time.time()
+                cnt = 0
+            except:
+                pass
+        cnt += 1
